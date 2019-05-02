1ff4e56719d6e4a635207fff63090a485abeeb09
Nikolas <nkrecu2211@stu.d214.org>
2019-05-02 11:34:42 -0500

    and desktop names

---
+++ NewTech/desktop.swift
@@ -0,0 +1,17 @@
+//
+//  desktop.swift
+//  NewTech
+//
+//  Created by Nikolas Krecu on 5/2/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+class desktop{
+    var name:String
+    init(name:String){
+        self.name = name
+        
+    }
+
+}
