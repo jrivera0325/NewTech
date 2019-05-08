b5b8485c82968afd65d5ccea6d5e224405f0c41f
Nikolas <nkrecu2211@stu.d214.org>
2019-05-08 11:07:09 -0500

    added tableviews and connected them to view controllers

---
+++ NewTech/laptopViewController.swift
@@ -0,0 +1,40 @@
+//
+//  laptopViewController.swift
+//  NewTech
+//
+//  Created by Nikolas Krecu on 5/7/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class laptopViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    @IBOutlet weak var tableView3: UITableView!
+    
+    override func viewDidLoad() {
+        tableView3.dataSource = self
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
