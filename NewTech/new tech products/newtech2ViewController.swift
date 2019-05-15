dfe3d954fefa95e92cd315f35f0eac302b82a14c
Nikolas <nkrecu2211@stu.d214.org>
2019-05-15 11:18:28 -0500

    added ibaction to buttons

--- NewTech/new tech products/newtech2ViewController.swift
+++ NewTech/new tech products/newtech2ViewController.swift
@@ -16,15 +16,9 @@ class newtech2ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func whenButton8Pressed(_ sender: Any) {
     }
-    */
+    
+   
 
 }
