9a903b9a0d235f1eb992ba97865635523076d900
Nikolas <nkrecu2211@stu.d214.org>
2019-05-16 11:30:07 -0500

    finished safari view controller

--- NewTech/coolgadget products/coolgadget3ViewController.swift
+++ NewTech/coolgadget products/coolgadget3ViewController.swift
@@ -18,7 +18,7 @@ class coolgadget3ViewController: UIViewController {
     }
     
     @IBAction func whenButton12Pressed(_ sender: Any) {
-        if let url = URL(string: "https://www.amazon.com/Monitor-UPERFECT-12-3-inch-Portable-1600×1200/dp/B07MV6X7ML/ref=sr_1_fkmrnull_1_sspa?gclid=EAIaIQobChMI2sKll7qg4gIVXbjACh1CKwecEAAYASAAEgIJJ_D_BwE&hvadid=323467243141&hvdev=c&hvlocphy=9021427&hvnetw=g&hvpos=1t1&hvqmt=e&hvrand=16466415940566589915&hvtargid=kwd-644201729658&hydadcr=731_1012416413&keywords=uperfect+portable+monitor&qid=1558023603&s=gateway&sr=8-1-fkmrnull-spons&psc=1"){
+        if let url = URL(string: "https://www.amazon.com/Portable-UPERFECT-Compatible-1920x1080-Raspberry/dp/B07CNNP9LN"){
             let safariViewController = SFSafariViewController(url: url)
             present(safariViewController, animated: true, completion: nil)
         }
