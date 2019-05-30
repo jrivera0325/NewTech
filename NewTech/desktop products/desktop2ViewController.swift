81a5eca8f4f0ea44d608f685762449730df71cd7
Nikolas <nkrecu2211@stu.d214.org>
2019-05-30 11:10:35 -0500

    tried fixing video problem

--- NewTech/desktop products/desktop2ViewController.swift
+++ NewTech/desktop products/desktop2ViewController.swift
@@ -11,15 +11,18 @@ import WebKit
 import SafariServices
 
 class desktop2ViewController: UIViewController {
+   
     @IBOutlet weak var webView2: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let dellUrl = URL(string: "https://www.youtube.com/watch?v=1VzfhgU3Ipk")
+        // https://www.youtube.com/watch?v=1VzfhgU3Ipk
+        let dellUrl = URL(string: "https://www.google.com/")
         webView2.load(URLRequest(url: dellUrl!))
-        // Do any additional setup after loading the view.
+
     }
+    
+   
     
     @IBAction func whenButton2Pressed(_ sender: Any) {
         if let url = URL(string: "https://www.bestbuy.com/site/dell-inspiron-desktop-intel-core-i7-12gb-memory-1tb-hard-drive-black-silver-trim/6229148.p?skuId=6229148"){
